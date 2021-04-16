188b71b2f6d4a88aae0f6ec9f4a5957a49b01c6d
LightningGrace <grace1hammond@gmail.com>
2021-04-16 22:07:53 +0100

    first commit

---
+++ eventPage.js
@@ -0,0 +1,50 @@
+const contextMenu = [
+	{
+		id: 'contextMenu',
+		title: 'Search Site',
+		contexts: ['selection']
+	},
+
+	{
+		id: 'searchGoogle',
+		title: 'Search Google',
+		contexts: ['selection'],
+		parentId: 'contextMenu'
+	},
+
+	{
+		id: 'searchBing',
+		title: 'Search Bing',
+		contexts: ['selection'],
+		parentId: 'contextMenu'
+	},
+
+	{
+		id: 'searchYouTube',
+		title: 'Search YouTube',
+		contexts: ['selection'],
+		parentId: 'contextMenu'
+	}
+];
+
+contextMenu.forEach(menuItem => chrome.contextMenus.create(menuItem));
+
+chrome.contextMenus.onClicked.addListener(clickQuery => {
+	if (!clickQuery.selectionText) return;
+
+	const tabCreator = (qURL, query) => {
+		chrome.tabs.create({url: `${qURL}${encodeURIComponent(query)}`});
+	};
+
+	switch (clickQuery.menuItemId) {
+		case 'searchGoogle':
+			tabCreator('https://www.google.com/search?q=', clickQuery.selectionText);
+			break;
+		case 'searchBing':
+			tabCreator('https://www.bing.com/search?q=', clickQuery.selectionText);
+			break;
+		case 'searchYouTube':
+			tabCreator('https://www.youtube.com/results?search_query=', clickQuery.selectionText);
+			break;
+	}
+});
